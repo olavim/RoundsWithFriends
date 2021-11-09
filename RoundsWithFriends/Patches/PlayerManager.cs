c34eccde3ae5b9d40f0d21eb07faab4a4965ca6d
pdcook <qphysicscook@gmail.com>
2021-11-09 00:26:53 -0500

    working on randomized spawns

--- RoundsWithFriends/Patches/PlayerManager.cs
+++ RoundsWithFriends/Patches/PlayerManager.cs
@@ -1,5 +1,10 @@
 ﻿using HarmonyLib;
 using Photon.Pun;
+using System.Reflection;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Sonigon;
 
 namespace RWF.Patches
 {
@@ -37,5 +42,43 @@ namespace RWF.Patches
             __result = __instance.GetClosestPlayerInOtherTeam(asker.transform.position, asker.teamID, false);
             return false;
         }
+    }
+
+    [HarmonyPatch(typeof(PlayerManager), "MovePlayers")]
+    class PlayerManager_Patch_MovePlayers
+    {
+        static bool Prefix(PlayerManager __instance, SpawnPoint[] spawnPoints)
+        {
+            __instance.StartCoroutine(WaitForMapToLoad(__instance, spawnPoints));
+            return false;
+        }
+        static IEnumerator WaitForMapToLoad(PlayerManager __instance, SpawnPoint[] spawnPoints)
+        {
+            yield return new WaitForSecondsRealtime(1f); // wait for map colliders to load
+
+            Dictionary<Player, Vector2> spawnDictionary = GeneralizedSpawnPositions.GetSpawnDictionary(__instance.players, spawnPoints);
+
+            for (int i = 0; i < __instance.players.Count; i++)
+            {
+
+                __instance.StartCoroutine((IEnumerator) typeof(PlayerManager).InvokeMember("Move",
+                    BindingFlags.Instance | BindingFlags.InvokeMethod |
+                    BindingFlags.NonPublic, null, __instance, new object[] { __instance.players[i].data.playerVel, (Vector3) spawnDictionary[__instance.players[i]] }));
+                /*
+                yield return (IEnumerator) typeof(PlayerManager).InvokeMember("Move",
+    BindingFlags.Instance | BindingFlags.InvokeMethod |
+    BindingFlags.NonPublic, null, __instance, new object[] { __instance.players[i].data.playerVel, (Vector3) spawnPositions[i] });
+                */
+                // I have no idea why this is in the original method like this but I'm afraid to change it
+                int j;
+                for (j = i; j >= __instance.soundCharacterSpawn.Length; j -= __instance.soundCharacterSpawn.Length)
+                {
+                }
+
+                SoundManager.Instance.Play(__instance.soundCharacterSpawn[j], __instance.players[i].transform);
+            }
+        }
+
+
     }
 }
