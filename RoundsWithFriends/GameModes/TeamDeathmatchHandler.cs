773faf50937f79221f4c6438e6068dc2032d8245
pdcook <qphysicscook@gmail.com>
2021-12-31 22:59:53 -0500

    added support for non-host disconnects. requires UnboundLib 2.10.0+

--- RoundsWithFriends/GameModes/TeamDeathmatchHandler.cs
+++ RoundsWithFriends/GameModes/TeamDeathmatchHandler.cs
@@ -1,5 +1,7 @@
 ﻿using UnboundLib;
 using UnboundLib.GameModes;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RWF.GameModes
 {
@@ -34,6 +36,61 @@ namespace RWF.GameModes
         public override void PlayerDied(Player player, int playersAlive)
         {
             GM_TeamDeathmatch.instance.PlayerDied(player, playersAlive);
+        }
+        public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+        {
+            List<Player> disconnected = PlayerManager.instance.players.Where(p => p.data.view.ControllerActorNr == otherPlayer.ActorNumber).ToList();
+
+            // get new teamIDs
+            Dictionary<int, List<Player>> teams = new Dictionary<int, List<Player>>() { };
+            foreach (Player player in PlayerManager.instance.players.Except(disconnected).OrderBy(p => p.teamID).ThenBy(p => p.playerID))
+            {
+                if (!teams.ContainsKey(player.teamID)) { teams[player.teamID] = new List<Player>() { }; }
+
+                teams[player.teamID].Add(player);
+            }
+
+            Dictionary<Player, int> newTeamIDs = new Dictionary<Player, int>() { };
+
+            int teamID = 0;
+            foreach (int oldID in teams.Keys)
+            {
+                foreach (Player player in teams[oldID])
+                {
+                    newTeamIDs[player] = teamID;
+                }
+                teamID++;
+            }
+
+            // update team scores
+            Dictionary<int, int> newTeamPoints = new Dictionary<int, int>() { };
+            Dictionary<int, int> newTeamRounds = new Dictionary<int, int>() { };
+
+            foreach (Player player in newTeamIDs.Keys)
+            {
+                if (!newTeamPoints.Keys.Contains(newTeamIDs[player]))
+                {
+                    newTeamPoints[newTeamIDs[player]] = GM_TeamDeathmatch.instance.teamPoints[player.teamID];
+                }
+                if (!newTeamRounds.Keys.Contains(newTeamIDs[player]))
+                {
+                    newTeamRounds[newTeamIDs[player]] = GM_TeamDeathmatch.instance.teamRounds[player.teamID];
+                }
+            }
+
+            GM_TeamDeathmatch.instance.teamPoints = newTeamPoints;
+            GM_TeamDeathmatch.instance.teamRounds = newTeamRounds;
+
+            // fix score counter
+            UIHandler.instance.roundCounter.GetData().teamPoints = newTeamPoints;
+            UIHandler.instance.roundCounter.GetData().teamRounds = newTeamRounds;
+            UIHandler.instance.roundCounterSmall.GetData().teamPoints = newTeamPoints;
+            UIHandler.instance.roundCounterSmall.GetData().teamRounds = newTeamRounds;
+
+            // UnboundLib handles PlayerManager fixing, which includes reassigning playerIDs and teamIDs
+            // as well as card bar fixing
+            base.OnPlayerLeftRoom(otherPlayer);
+
         }
 
         public override TeamScore GetTeamScore(int teamID)
