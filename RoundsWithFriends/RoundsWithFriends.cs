f32a0d3ca581ccf635cc4116fd595961c18e5bbb
pdcook <qphysicscook@gmail.com>
2022-01-03 11:44:03 -0500

    v2.1.2

--- RoundsWithFriends/RoundsWithFriends.cs
+++ RoundsWithFriends/RoundsWithFriends.cs
@@ -61,7 +61,7 @@ namespace RWF
         private const string ModName = "Rounds With Friends";
         private static string CompatibilityModName => RWFMod.ModName.Replace(" ", "");
         private const string ModId = "io.olavim.rounds.rwf";
-        public const string Version = "2.1.1";
+        public const string Version = "2.1.2";
 
 #if DEBUG
         public static readonly bool DEBUG = true;
